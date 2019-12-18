cbfe7c1dc8476534b3485a7c85a0bc4e9078a5f3
Atinder <>
2019-12-18 15:56:47 +0900

    Minor Changes

---
+++ Stage/Stage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stage
+//
+//  Created by IMPUTE on 18/12/19.
+//  Copyright © 2019 Atinder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
